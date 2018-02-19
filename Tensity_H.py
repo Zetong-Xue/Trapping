cf9d9fd87e3e9679a807dd32e4912670794203d4
Zetong-Xue <frostge95@gmail.com>
2018-02-18 18:24:51 -0700

    thir commit

--- Tensity_H.py
+++ Tensity_H.py
@@ -2,23 +2,22 @@ import matplotlib.pyplot as plt
 import matplotlib.transforms as transforms
 import numpy as np; np.random.seed(42)
 
-
 for i in range(12,16,1):
     plt.axhline(10**i, color='k', linestyle='--')
 
 
 for i in range(1,25,1):
 
-    x = np.arange(100,1000, 1) 
-    y = (4*(45.6/x)*(45.6/x)*(i * 45.6/x + -0.903849))/(2.8*10**-17)
-    plt.ylim(10**12 ,10**16)
+    x = np.arange(100,1000, 1)
+    y = (4*(45.6/x)*(45.6/x)*(i * 45.6/x + -0.5))/(2.8*10**-17)
+    plt.ylim(10**12,10**15)
     plt.semilogy(x, y, label = i)
     plt.legend(bbox_to_anchor=(1, 1),
            bbox_transform=plt.gcf().transFigure)
 
 
-    
-plt.title('He Intensity vs $\lambda$')
-plt.ylabel('I(w/cm^2)') 
+
+plt.title('H  Intensity vs $\lambda$')
+plt.ylabel('I(w/cm^2)')
 plt.xlabel('wavelength (nm)')
 plt.show()
--- Tensity_Ne.py
+++ Tensity_Ne.py
@@ -2,18 +2,22 @@ import matplotlib.pyplot as plt
 import matplotlib.transforms as transforms
 import numpy as np; np.random.seed(42)
 
+for i in range(12,16,1):
+    plt.axhline(10**i, color='k', linestyle='--')
+
 
 for i in range(1,25,1):
 
     x = np.arange(100,1000, 1)
     y = (4*(45.6/x)*(45.6/x)*(i * 45.6/x + -0.830605))/(2.8*10**-17)
     plt.ylim(10**12,10**15)
-    plt.plot(x, y, label = i)
-    plt.legend()
+    plt.semilogy(x, y, label = i)
+    plt.legend(bbox_to_anchor=(1, 1),
+           bbox_transform=plt.gcf().transFigure)
 
 
 
-plt.title('Ne')
+plt.title('Ne Intensity vs $\lambda$')
 plt.ylabel('I(w/cm^2)')
 plt.xlabel('wavelength (nm)')
 plt.show()
